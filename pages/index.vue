7a41a91e831210c29b34769551c0a5b5152e2782
liguob666 <47095363+liguob666@users.noreply.github.com>
2024-09-22 18:01:18 +0800

    Update index.vue
    修改为本地

--- pages/index.vue
+++ pages/index.vue
@@ -57,7 +57,7 @@ const onFileChange = (event) => {
   reader.readAsDataURL(file)
 }
 
-const watermarkText = ref(locale.value === 'cn' ? '仅供 xxx 验证使用' : 'Only for xxx verification use')
+const watermarkText = ref(locale.value === 'cn' ? '(13730856867)成都曹操跑腿' : 'Only for xxx verification use')
 const watermarkColor = ref('#0000ff')
 const watermarkOpacity = ref(0.3)
 const watermarkSpacing = ref(5)
